3266cdf705acf2db00742dbfb39f62fd5b15d388
Tanmay-Joshi73 <tanmayjoshi072@gmail.com>
2024-06-18 16:40:33 +0530

    80% work is completed

--- app.js
+++ app.js
@@ -1,13 +1,22 @@
 const express=require('express')
+const bodyparser = require('body-parser')
+const dotevn=require('dotenv').config()
+const PORT=process.env.PORT
 const fs=require('fs')
 const app=express()
 const path=require('path')
+app.use('/public',express.static(path.join(`${__dirname}`, 'public'))) 
+app.use(bodyparser.urlencoded({ extended: true }))
+app.use(express.json())
 const {render,fetch}=require('./controllers/controllers')
+const bodyParser = require('body-parser')
+app.get('/',(req,res)=>{
+    res.redirect('/Weather')
+})
 app.get('/Weather',render)
 app.post('/Fetch',fetch)
-app.use(express.static(path.join(__dirname, 'public'))) 
 
 
-app.listen(8000,'127.0.0.1',()=>{
+app.listen(PORT,'127.0.0.1',()=>{
     console.log("Listenig to the server")
 })
--- controllers/controllers.js
+++ controllers/controllers.js
@@ -1,10 +1,30 @@
-const express=require('express')
-const fs=require('fs')
-let file=fs.readFileSync(`${__dirname}/../weather.html`,'utf-8')
+const express = require('express')
+const axios = require('axios');
 
-exports.render=async(req,res)=>{
-res.send(file)
+const fs = require('fs');
+const { info } = require('console');
+const { addAbortSignal } = require('stream');
+let file = fs.readFileSync(`${__dirname}/../weather.html`, 'utf-8')
+API = 'https://api.openweathermap.org/data/2.5/weather?q={city name}&appid=107c8523554984e13e6f4a5915fde75c'
+exports.render = async (req, res) => {
+    res.send(file)
 }
-exports.fetch=async(req,res)=>{
-res.send('hey it is working my boi')
+
+exports.fetch = async (req, res) => {
+    const { city } = req.body
+    const Info = API.replace('{city name}', city)
+
+    const ans = await axios.get(Info)
+        .then(response => {
+            const data = response.data
+            return data
+        });
+
+    const CityName = ans.name
+    const Temp = ''+ ans.main.temp
+    const Humidity=''+ans.main.humidity
+    const Details={CityName:CityName,Tempreture:Temp,Humidity:Humidity}
+    res.setHeader("Content-Type","application/json")
+    res.send(Details)
+
 }
