94a2792d21b514d807ae01a9e6e7701f68be84ca
Tanmay-Joshi73 <tanmayjoshi072@gmail.com>
2024-06-19 12:43:27 +0530

    all work is done

--- controllers/controllers.js
+++ controllers/controllers.js
@@ -5,13 +5,16 @@ const fs = require('fs');
 const { info } = require('console');
 const { addAbortSignal } = require('stream');
 let file = fs.readFileSync(`${__dirname}/../weather.html`, 'utf-8')
-API = 'https://api.openweathermap.org/data/2.5/weather?q={city name}&appid=107c8523554984e13e6f4a5915fde75c'
+// API = 'https://api.openweathermap.org/data/2.5/weather?q={city name}&appid=107c8523554984e13e6f4a5915fde75c'
+API=process.env.API_Info
+
 exports.render = async (req, res) => {
     res.send(file)
 }
 
 exports.fetch = async (req, res) => {
     const { city } = req.body
+    
     const Info = API.replace('{city name}', city)
 
     const ans = await axios.get(Info)
