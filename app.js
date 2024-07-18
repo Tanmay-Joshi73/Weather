ad3263026e1546c4a5e09ac8df30164fcfdd84d0
Tanmay-Joshi73 <tanmayjoshi072@gmail.com>
2024-07-18 22:52:16 +0530

    minor changes

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.use(bodyparser.urlencoded({ extended: true }))
 app.use(express.json())
 app.use('',routes)
 app.use(ShowError)
-app.listen(PORT,'127.0.0.1',()=>{
+app.listen(PORT,'0.0.0.0',()=>{
     console.log("Listenig to the server")
     
 })
